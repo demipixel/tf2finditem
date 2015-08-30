647075c31d799dca7c9dae67a413f35357c8f414
DemiPixel <luke5227@gmail.com>
2015-08-30 15:23:07 -0700

    New target

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ MongoDB.on('error', function(err) {
 
 MongoDB.once('open', function() {
 	console.log('Connected to database');
-	searchId('76561198119415095');
+	searchId('76561198044193282');
 });
 
 var db = {
